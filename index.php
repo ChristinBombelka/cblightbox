c5906085c401538cbd542f749f5bcb866dea89a4
Christin Bombelka <christin-bom@web.de>
2019-04-30 19:14:37 +0200

    add viewport to header

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ $images2[] = $image;
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes">
 	<link  type="text/css" href="styles/cblightbox.css" rel="stylesheet"></link >
 	<link  type="text/css" href="styles/style.css" rel="stylesheet"></link >
 </head>

2279c2559b62a3009cc82f58a154a6c5ccf71221
Christin Bombelka <christin-bom@web.de>
2019-09-18 17:55:53 +0200

    update calc scrollbar width

--- scripts/cblightbox.js
+++ scripts/cblightbox.js
@@ -30,23 +30,29 @@
 			$("html").addClass("cb-lightbox-no-touch");
 		}
 
-		//calc screenBorderWidth
-		function scrollbarWidth() {
-		    var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>');
-		    // Append our div, do our calculation and then remove it
-		    $('body').append(div);
-		    var w1 = $('div', div).innerWidth();
+		function getScrollbarWidth() {
+			// Creating invisible container
+			const outer = document.createElement('div');
+			outer.style.visibility = 'hidden';
+			outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+			outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+			document.body.appendChild(outer);
 
-		    div.css('overflow-y', 'scroll');
+			// Creating inner element and placing it in the container
+			const inner = document.createElement('div');
+			outer.appendChild(inner);
 
-		    var w2 = $('div', div).innerWidth();
+			// Calculating difference between container's full width and the child width
+			const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
 
-		    $(div).remove();
-		    return (w1 - w2);
+			// Removing temporary elements from the DOM
+			outer.parentNode.removeChild(outer);
+
+			return scrollbarWidth;
 		}
 
 		if(!$(".cb-lightbox-margin").length){
-			$("<style type='text/css'>.cb-lightbox-margin{margin-right:"+scrollbarWidth()+"px;}</style>").appendTo($("head"));
+			$("<style type='text/css'>.cb-lightbox-margin{margin-right:" + getScrollbarWidth() + "px;}</style>").appendTo($("head"));
 		}
 
 		var defaults = {

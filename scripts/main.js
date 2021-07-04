08a451dbbc0c428ba62449f9083545132e87bdcd
Bombelka <christin-bom@web.de>
2021-07-04 16:18:23 +0200

    update demo files

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,11 @@ $(function() {
     $(".js-lightbox").cblightbox({
         margin: [50, 60, 100, 60],
         openCloseEffect: 'zoom',
-        openCloseDuration: 750,
+        openCloseDuration: 1000,
         slideEffect: 'slide',
         slideDuration: 300,
         zoom: true,
         zoomOffset : 0,
-        previewSource : 'data-original',
         captionPosition: 'inside',
         zoomControlls: true,
         zoomMap: true
